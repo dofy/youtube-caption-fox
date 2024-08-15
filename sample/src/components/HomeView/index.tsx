a1fe1847b0737e0d1d8640f66a741b8e6872d124
Seven Yu <dofyyu@gmail.com>
2024-08-15 22:38:12 +0800

    fetch

--- sample/src/components/HomeView/index.tsx
+++ sample/src/components/HomeView/index.tsx
@@ -5,12 +5,23 @@ import { FormView } from '../FormView'
 import { HeaderView } from '../HeaderView'
 import { ResultView } from '../ResultView'
 import type { FormData } from '../types'
+import { getCaptions } from '@dofy/youtube-caption-fox'
 
 export const HomeView: FC = () => {
+  const onSubmit = (data: FormData) => {
+    getCaptions(data.videoId, { lang: data.lang })
+      .then((captions) => {
+        console.log(captions)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
   return (
     <main className="mx-auto flex max-w-screen-lg flex-col gap-8 p-8">
       <HeaderView />
-      <FormView onSubmit={(data: FormData) => console.log(data)} />
+      <FormView onSubmit={onSubmit} />
       <ResultView />
     </main>
   )
