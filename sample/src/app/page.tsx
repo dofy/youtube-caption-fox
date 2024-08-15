9789211869df704b9b53868fba6c946399c50b21
Seven Yu <dofyyu@gmail.com>
2024-08-15 11:25:22 +0800

    update page

--- sample/src/app/page.tsx
+++ sample/src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>s</div>;
+  return (
+    <main className="w-full p-4 flex flex-col gap-4">
+      <section className="mx-auto md:w-2/3 w-full p-4 bg-black/10 rounded-lg">
+        <div>form</div>
+      </section>
+      <section className="mx-auto md:w-2/3 w-full p-4 bg-blue-300/10 rounded-lg">
+        <div>result</div>
+      </section>
+    </main>
+  );
 }
